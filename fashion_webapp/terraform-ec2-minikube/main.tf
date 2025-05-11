89b6ae5a60f421359f331763b8c5d2a881b202ca
hashbury1 <efefiomarchibong@gmail.com.com>
2025-05-11 09:18:58 +0100

    update on my main tf with egress rule

--- fashion_webapp/terraform-ec2-minikube/main.tf
+++ fashion_webapp/terraform-ec2-minikube/main.tf
@@ -38,12 +38,29 @@ resource "aws_security_group" "minikube_sg" {
   vpc_id      = data.aws_vpc.default.id
 
 
-  egress {
+  ingress {
     from_port = 22
     to_port   = 22
     protocol = "tcp" # All protocols
     cidr_blocks = ["29.222.206.136/32"]
   }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outbound HTTP traffic"
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outbound HTTPS traffic"
+  }
+
 }
 
   data "aws_vpc" "default" {
